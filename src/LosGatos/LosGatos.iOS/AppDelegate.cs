c35712a51f6b2f725eec7da554fbfacd283decf8
davidortinau <david.ortinau@microsoft.com>
2020-11-08 10:53:30 -0600

    more content, shadow work, WIP

--- src/LosGatos/LosGatos.iOS/AppDelegate.cs
+++ src/LosGatos/LosGatos.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace LosGatos.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Sharpnado.Shades.iOS.iOSShadowsRenderer.Initialize();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
