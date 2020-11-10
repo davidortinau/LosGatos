184f751e72ea507d38dda9b14301a0d1b51b1e18
davidortinau <david.ortinau@microsoft.com>
2020-11-10 08:12:39 -0600

    android layout tweaks, app icon, splash

--- src/LosGatos/LosGatos.Android/MainActivity.cs
+++ src/LosGatos/LosGatos.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Xamarin.Forms.Platform.Android;
 
 namespace LosGatos.Droid
 {
-    [Activity(Label = "LosGatos", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "LosGatos", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ src/LosGatos/LosGatos.Android/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+using AndroidX.AppCompat.App;
+
+namespace LosGatos.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", Label = "Los Gatos", Icon = "@mipmap/icon", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            Log.Debug(TAG, "Performing some startup work that takes a bit of time.");
+            //await Task.Delay(1000); // Simulate a bit of startup work.
+            Log.Debug(TAG, "Startup work is finished - starting MainActivity.");
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
