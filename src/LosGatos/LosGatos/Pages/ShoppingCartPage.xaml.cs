146fd9a4e3477db762337f6787ba00512743d0fc
davidortinau <david.ortinau@microsoft.com>
2021-01-13 11:12:51 -0600

    added alert to shopping cart swipe actions

--- src/LosGatos/LosGatos/Pages/ShoppingCartPage.xaml.cs
+++ src/LosGatos/LosGatos/Pages/ShoppingCartPage.xaml.cs
@@ -16,11 +16,13 @@ namespace LosGatos.Pages
         {
             // Remove from cart
             // Add to saved for later
+            DisplayAlert("Yay!", "If this were a real app, we might do something. It's not.", "Okay!");
         }
 
         void OnRemoveButtonClicked(object sender, EventArgs e)
         {
             // Remove from cart
+            DisplayAlert("Yay!", "If this were a real app, we might do something. It's not.", "Okay!");
         }
     }
 }
